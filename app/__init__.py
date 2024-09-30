de79d1f3ac1d963a2abfa86243613f5e8f4c6488
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-09-30 22:33:15 +0530

    Compleated Project

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 from flask import Flask
 
 app = Flask(__name__)
 
 # Import routes after initializing the app
 from app import routes
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+# Import routes after initializing the app
+from app import routes
+>>>>>>> ff399a1 (Compleated Project)
